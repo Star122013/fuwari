100977f3a790da83a6bfca739bda5a7fa8fbfe2f
Star122013 <hyy122013@outlook.com>
2024-10-15 22:47:54 +0800

    完善

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://heyanyu.fun",
   base: "/",
   trailingSlash: "always",
   integrations: [
